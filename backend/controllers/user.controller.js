bf8124af664d8095a0e3a86221112bb1c66058b2
svargasc <117184778+svargasc@users.noreply.github.com>
2024-02-02 08:59:45 -0500

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -61,13 +61,11 @@ export const login = async (req, res) => {
       });
 
       // Establecer cookie con el token
-      res.cookie("token", token, {
+      return res.cookie("token", token, {
         domain: 'events-cqtw.onrender.com',
         path: '/',
         // otras opciones de cookie si es necesario
       });
-
-      return res.json({ Status: "Success" , token: token});
     } else {
       return res.json({ Error: "Password not matched" });
     }
