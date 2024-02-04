072b5f3fe5038c9e5d8063b261d0b467af3c776c
svargasc <svc.vargas03@gmail.com>
2024-02-03 19:15:59 -0500

    added

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://clever-parfait-80b841.netlify.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
