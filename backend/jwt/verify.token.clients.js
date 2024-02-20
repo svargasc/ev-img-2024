e2a618cd6486b409b08a0a13453c5c3b957dc4b1
unknown <SenaManana@ARMAPRRQDFSD112.FormacionSena.RED>
2024-02-20 06:30:01 -0500

    added res.json in clients.controller

--- backend/jwt/verify.token.clients.js
+++ backend/jwt/verify.token.clients.js
@@ -2,7 +2,42 @@ import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
 import { pool } from "../db/db.js";
 
-export const verifyClients = async (req, res, next) => {
+// export const verifyClients = async (req, res, next) => {
+//   const authorizationHeader = req.headers["authorization"];
+
+//   console.log("Token en los headers cuando se verifica", authorizationHeader);
+//   if (!authorizationHeader) {
+//     return res.status(401).json({ message: "Unauthorized 1" });
+//   }
+
+//   const token = authorizationHeader.split(" ")[1];
+
+//   jwt.verify(token, TOKEN_SECRET, async (err, decoded) => {
+//     if (err) {
+//       return res.status(401).json({ message: "Unauthorized 2" });
+//     }
+
+//     try {
+//       const id = decoded.id;
+//       const clientQuery = "SELECT * FROM clients WHERE id = ?";
+//       const [clientData] = await pool.query(clientQuery, [id]);
+
+//       if (clientData.length === 0) {
+//         return res.status(404).json({ message: "Client not found" });
+//       }
+
+//       const client = clientData[0];
+//       req.client = client;
+//       res.json({ message: "ok" })
+//       next();
+//     } catch (error) {
+//       console.error("Error verifying token:", error);
+//       return res.status(500).json({ message: "Internal Server Error" });
+//     }
+//   });
+// };
+
+export const verifyToken = async (req, res, next) => {
   const authorizationHeader = req.headers["authorization"];
 
   console.log("Token en los headers cuando se verifica", authorizationHeader);
@@ -20,15 +55,15 @@ export const verifyClients = async (req, res, next) => {
     try {
       const id = decoded.id;
       const clientQuery = "SELECT * FROM clients WHERE id = ?";
-      const [clientData] = await pool.query(clientQuery, [id]);
+      const [usertData] = await pool.query(clientQuery, [id]);
 
-      if (clientData.length === 0) {
-        return res.status(404).json({ message: "Client not found" });
+      if (userQuery.length === 0) {
+        return res.status(404).json({ message: "User not found" });
       }
 
       const client = clientData[0];
       req.client = client;
-      res.json({ message: "ok" })
+      res.json({message: "ok"})
       next();
     } catch (error) {
       console.error("Error verifying token:", error);
