60dd95b27e2705162aa7a83897fd87ce2052be1b
svargasc <117184778+svargasc@users.noreply.github.com>
2024-03-03 21:33:17 -0500

    Create imagesIA.controller.js

---
+++ backend/controllers/imagesIA.controller.js
@@ -0,0 +1,129 @@
+import multer from "multer";
+import { pool } from "../db/db.js";
+import path from "path";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "fs";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+export const upload = multer({ storage: storage });
+
+// Función para procesar la imagen y actualizar la base de datos según la respuesta
+async function run(images, eventId, res) {
+  try {
+    const API_KEY_GEMINI = "AIzaSyBC2HGD0k0nn3ElSvHd01iI6wdnz8Ri_mM";
+    const genAI = new GoogleGenerativeAI(API_KEY_GEMINI);
+
+    const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+    const objeto = "Cafe, catacion, restaurantes, naturaleza o cafeterias";
+    const prompt = `Responde solo con Sí o solo con No en caso de que se encuentre o no se encuentre el objeto por el que te preguntan. ¿En la imagen hay un ${objeto}?`;
+
+    // Procesa cada imagen individualmente
+    for (const image of images) {
+      const imageFileName = image[1]; // El segundo elemento del array es el nombre del archivo
+      const imagePath = `public/uploads/${imageFileName}`;
+
+      const imageParts = [
+        {
+          inlineData: {
+            data: Buffer.from(fs.readFileSync(imagePath)).toString("base64"),
+            mimeType: "image/jpeg",
+          },
+        },
+      ]; 
+
+      const result = await model.generateContent([prompt, ...imageParts]);
+      const response = await result.response;
+      const text = await response.text();
+
+      let resultado;
+      if (text.trim() === "Sí" || text.trim() === "Sí hay") {
+        resultado = 1;
+        console.log("Se encontró el objeto en la imagen:", text);
+        await createEventImage(eventId, imageFileName);
+      } else if (text.trim() === "No") {
+        resultado = 0;
+        console.log("No se encontró el objeto en la imagen:", text);
+        await deleteEventImage(res, eventId);
+        return;
+      } else {
+        console.log("Respuesta desconocida:", text);
+        await createEventImage(eventId, imageFileName);
+        return;
+      }
+
+      console.log("Resultado:", resultado);
+    }
+    return res.json({ Status: "Success" });
+  } catch (error) {
+    console.error("Error in run:", error);
+  }
+}  
+
+// Función para actualizar la imagen en la base de datos
+async function createEventImage(eventId, imageValues) {
+  try {
+    // Ejecuta la consulta para insertar las nuevas imágenes en la base de datos
+    const insertQuery =
+      "INSERT INTO event_images (event_id, image_url) VALUES (?, ?)";
+    await pool.query(insertQuery, [eventId, imageValues]);
+    console.log("Imagen actualizada en la base de datos");
+    // return res.json({ Status: "Success", imageValues });
+  } catch (error) {
+    console.error("Error updating event image:", error);
+    res.status(500).json({ message: "Error creating event image" });
+  }
+}
+ 
+// Función para eliminar la imagen de la base de datos
+async function deleteEventImage(res, eventId) {
+  try {
+    await pool.query("DELETE FROM event_images WHERE event_id = ?", [eventId]);
+    console.log("Imagen eliminada de la base de datos");
+    // Enviar la respuesta al cliente desde aquí
+    return res.json({ message: "Image deleted" });
+  } catch (error) {
+    console.error("Error deleting event image:", error);
+    return res.status(500).json({ message: "Error deleting event image" });
+  }
+}
+
+// Controlador para subir imágenes y procesarlas
+export const createEventImageHandler = async (req, res) => {
+  try {
+    const eventId = req.body.eventId; // Obtiene el ID del evento desde la solicitud
+    const images = req.files.map((file) => file.filename); // Obtiene los nombres de archivo de las imágenes cargadas
+
+    if (!eventId) {
+      return res.status(400).json({ message: "Event ID is required" });
+    }
+
+    // Verifica si se cargaron imágenes
+    if (!images || images.length === 0) {
+      return res.status(400).json({ message: "No images uploaded" });
+    }
+
+    // Elimina las imágenes anteriores asociadas al evento
+    await pool.query("DELETE FROM event_images WHERE event_id = ?", [eventId]);
+
+    // Prepara los valores de las nuevas imágenes para la inserción en la base de datos
+    const imageValues = images.map((image) => [eventId, image]);
+
+    // Llama a la función run con los valores de las imágenes y el ID del evento
+    await run(imageValues, eventId, res);
+
+  } catch (error) {
+    console.error("Error in createEventImageHandler:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
