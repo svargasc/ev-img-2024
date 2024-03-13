da84360b75383860d2fec026db3567853b6e176b
unknown <SenaManana@ARMAPRRQDFSD112.FormacionSena.RED>
2024-03-13 10:25:11 -0500

    added a

--- backend/controllers/imgUser.controller.js
+++ backend/controllers/imgUser.controller.js
@@ -20,26 +20,29 @@ export const upload = multer({ storage: storage });
 
 export const updateImageProfile = async (req, res) => {
     try {
-        // const userId = req.body.userId;
-        // const image = req.file.filename;
-        // req.body;
-        // const sql = "UPDATE users SET ? WHERE id = ?";
-        // await pool.query(sql, [image, userId]);
-        // return res.json({ Status: "Success Update Profile" });
-        const updates = {
-            username: req.body.username,
-            email: req.body.email,
-            img_profile: req.file.filename,
-        };
-        const result = await pool.query("UPDATE users SET ? WHERE id = ?", [
-            updates,
-            req.params.id,
-        ]);
-        if (!userId) {
-            return res.status(400).json({ message: "User ID is required" });
-        }
-        res.json(result, {Status: "Success Update Profile"});
+        const userId = req.body.userId;
+        const username = req.body.username;
+        const email = req.body.email;
+        const image = req.file.filename;
+        
+        const sql = "UPDATE users SET ? WHERE id = ?";
+        await pool.query(sql, [userId, username, email, image]);
         console.log("Actulizacion correcta");
+        return res.json({ Status: "Success Update Profile" });
+        // const updates = {
+        //     username: req.body.username,
+        //     email: req.body.email,
+        //     img_profile: req.file.filename,
+        // };
+        // const result = await pool.query("UPDATE users SET ? WHERE id = ?", [
+        //     updates,
+        //     req.params.id,
+        // ]);
+        // if (!userId) {
+        //     return res.status(400).json({ message: "User ID is required" });
+        // }
+        // res.json(result, {Status: "Success Update Profile"});
+        // console.log("Actulizacion correcta");
 
     } catch (error) {
         console.error("Error in updateEventImageHandler:", error);
