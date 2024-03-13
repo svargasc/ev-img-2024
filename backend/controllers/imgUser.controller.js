9345ed28af4944e496f5ba326c27490c8344bf86
unknown <SenaManana@ARMAPRRQDFSD112.FormacionSena.RED>
2024-03-13 07:15:31 -0500

    added

--- backend/controllers/imgUser.controller.js
+++ backend/controllers/imgUser.controller.js
@@ -5,34 +5,35 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "public/uploads");
+        cb(null, "public/uploads");
     },
     filename: (req, file, cb) => {
-      cb(
-        null,
-        file.fieldname + "_" + Date.now() + path.extname(file.originalname)
-      );
+        cb(
+            null,
+            file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+        );
     },
-  });
-  
-  export const upload = multer({ storage: storage });
+});
+
+export const upload = multer({ storage: storage });
 
 
-  export const updateImageProfile = async (req, res) => {
+export const updateImageProfile = async (req, res) => {
     try {
-      const userId = req.body.userId;
-      const image = req.file.filename;
-        
-      if (!userId) {
-        return res.status(400).json({ message: "User ID is required" });
-      }
+        const userId = req.body.userId;
+        const image = req.file.filename;
 
-      const sql = "UPDATE users SET img_profile = ? WHERE id = ?";
-      await pool.query(sql, [image, userId]);
-      console.log("Imagen actualizada en la base de datos");
-      
+        if (!userId) {
+            return res.status(400).json({ message: "User ID is required" });
+        }
+
+        const sql = "UPDATE users SET img_profile = ? WHERE id = ?";
+        await pool.query(sql, [image, userId]);
+        console.log("Imagen actualizada en la base de datos");
+        return res.json({ Status: "Success" });
+
     } catch (error) {
-      console.error("Error in updateEventImageHandler:", error);
-      return res.status(500).json({ message: "Internal Server Error" });
+        console.error("Error in updateEventImageHandler:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
-  };
+};
