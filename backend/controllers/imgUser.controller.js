b37928977c47aca6678e7f08c3c8c61f2b7ec03c
unknown <SenaManana@ARMAPRRQDFSD112.FormacionSena.RED>
2024-03-13 09:31:24 -0500

    added

--- backend/controllers/imgUser.controller.js
+++ backend/controllers/imgUser.controller.js
@@ -20,17 +20,21 @@ export const upload = multer({ storage: storage });
 
 export const updateImageProfile = async (req, res) => {
     try {
-        const userId = req.body.userId;
-        const image = req.file.filename;
-
+        // const userId = req.body.userId;
+        // const image = req.file.filename;
+        // req.body;
+        // const sql = "UPDATE users SET ? WHERE id = ?";
+        // await pool.query(sql, [image, userId]);
+        // return res.json({ Status: "Success Update Profile" });
+        const result = await pool.query("UPDATE users SET ? WHERE id = ?", [
+            req.body,
+            req.req.body.userId,
+        ]);
         if (!userId) {
             return res.status(400).json({ message: "User ID is required" });
         }
-
-        const sql = "UPDATE users SET img_profile = ? WHERE id = ?";
-        await pool.query(sql, [image, userId]);
-        console.log("Imagen actualizada en la base de datos");
-        return res.json({ Status: "Success" });
+        res.json(result, {Status: "Success Update Profile"});
+        console.log("Actulizacion correcta");
 
     } catch (error) {
         console.error("Error in updateEventImageHandler:", error);
