97f1af1be24f998f9fd4553ea405d7279e3a02a3
svargasc <117184778+svargasc@users.noreply.github.com>
2024-04-02 17:13:04 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://eventsbrewssj.netlify.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
